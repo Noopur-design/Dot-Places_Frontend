c4b30973c6d66fd747093656671faa9dfd2547b5
Noopur <noorweb3@outlook.com>
2025-05-19 14:47:18 +0530

    Initial commit

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import './Map.css';
+
+// Fix for default icon issue in Leaflet + Webpack
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const Map = props => {
+  const { center, zoom } = props;
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      className={`map ${props.className}`}
+      style={props.style}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={center} />
+    </MapContainer>
+  );
+};
+
+export default Map;
